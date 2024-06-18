28c5a735091777cb44d654acace015b839b8c66b
itlazykin <dzaka89@gmail.com>
2024-06-18 16:14:05 +0300

    21. Вывод текста из нескольких строк в рамке из "звёздочек"

---
+++ src/main/java/ru/job4j/io/Task21.java
@@ -0,0 +1,12 @@
+package ru.job4j.io;
+
+public class Task21 {
+    public static void main(String[] args) {
+        String fullName = "Лазыкин Денис";
+        String phoneNumber = "+ 7(926) 888-23-28";
+        System.out.print("************************\n");
+        System.out.println("*    " + fullName + "     *");
+        System.out.println("*  " + phoneNumber + "  *");
+        System.out.print("************************\n");
+    }
+}
