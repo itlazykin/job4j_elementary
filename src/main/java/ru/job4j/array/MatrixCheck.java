7745c86bfe44f1d434ced6c833e124333d9fb722
itlazykin <dzaka89@gmail.com>
2024-06-20 12:28:54 +0300

    6.7.2. Моно столбец в матрице. [#214127 #238669]

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -1,10 +1,37 @@
 package ru.job4j.array;
 
 public class MatrixCheck {
+    /**
+     * Метод проверяет то, что строка в двухмерном массиве целиком заполнена символом 'X'.
+     * При этом в массиве могут содержаться только символ пробела ' ' и X - 'X'.
+     *
+     * @param board массив.
+     * @param row   строка.
+     * @return true - если строка заполнена Х.
+     */
     public static boolean monoHorizontal(char[][] board, int row) {
         boolean result = true;
         for (int i = 0; i < board.length; i++) {
             if (board[row][i] != 'X') {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Метод проверяет то, что колонка в двухмерном массиве целиком заполнена символом 'X'.
+     * При этом в массиве могут содержаться только символ пробела ' ' и X - 'X'.
+     *
+     * @param board  массив
+     * @param column колонка
+     * @return true - если колонка заполнена Х.
+     */
+    public static boolean monoVertical(char[][] board, int column) {
+        boolean result = true;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][column] == ' ') {
                 result = false;
                 break;
             }
