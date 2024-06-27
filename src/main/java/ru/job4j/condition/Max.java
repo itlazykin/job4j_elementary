c1abaae0fef26e6838bf0a902a406b87581bc570
itlazykin <dzaka89@gmail.com>
2024-06-27 09:52:42 +0300

    3. Перегрузить метод max для трех чисел [#122644 #249531]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -5,10 +5,20 @@ public class Max {
         return left > right ? left : right;
     }
 
+    public static int max(int first, int second, int third) {
+        return max(first, max(second, third));
+    }
+
+    public static int max(int first, int second, int third, int four) {
+        return max(max(first, second), max(third, four));
+    }
+
     public static void main(String[] args) {
         System.out.println(max(5, 6));
         System.out.println(max(6, 5));
         System.out.println(max(6, 6));
         System.out.println(max(-1, 6));
+        System.out.println(max(10, 20, 30));
+        System.out.println(max(10, 20, 30, 40));
     }
 }
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -31,4 +31,35 @@ class MaxTest {
         int expected = 2;
         assertThat(result).isEqualTo(expected);
     }
+
+    @Test
+    void whenMax10To20To30Then30() {
+        int first = 10;
+        int second = 20;
+        int third = 30;
+        int result = Max.max(first, second, third);
+        int expected = 30;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenMax20To30To10Then30() {
+        int first = 20;
+        int second = 30;
+        int third = 10;
+        int result = Max.max(first, second, third);
+        int expected = 30;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenMax20To30To10To40Then40() {
+        int first = 20;
+        int second = 30;
+        int third = 10;
+        int four = 40;
+        int result = Max.max(first, second, third, four);
+        int expected = 40;
+        assertThat(result).isEqualTo(expected);
+    }
 }
