a438f0244687d822640c19d928dfef0fe2a6428b
itlazykin <dzaka89@gmail.com>
2024-08-21 13:02:06 +0300

    21. Вывод текста из нескольких строк в рамке из "звёздочек"

--- src/main/java/ru/job4j/io/Task21.java
+++ src/main/java/ru/job4j/io/Task21.java
@@ -1,12 +1,19 @@
 package ru.job4j.io;
 
+/**
+ * Напишите класс, который выводит на экран в одной строке ваше фамилию, имя, отчество,
+ * в другой ― телефон, все это ― в рамке из «звездочек».
+ *
+ */
 public class Task21 {
     public static void main(String[] args) {
-        String fullName = "Лазыкин Денис";
-        String phoneNumber = "+ 7(926) 888-23-28";
-        System.out.print("************************\n");
-        System.out.println("*    " + fullName + "     *");
-        System.out.println("*  " + phoneNumber + "  *");
-        System.out.print("************************\n");
+        String fullName = "Фамилия Имя Отчество";
+        String phoneNumber = "+7(938) 123-45-67";
+        String stars = "************************";
+        String star = "*";
+        System.out.print(stars + System.lineSeparator()
+                + star + " " + fullName + " " + star + System.lineSeparator()
+                + star + " " + phoneNumber + "    " + star + System.lineSeparator()
+                + stars);
     }
 }
