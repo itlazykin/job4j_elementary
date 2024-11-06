3551a2b5dcbb2f5d6c3a3de39bd76ae99219a5c9
itlazykin <dzaka89@gmail.com>
2024-11-06 10:42:47 +0300

    Учу sql на stepik. Выборка данных по условию.

--- db/Stepik.sql
+++ db/Stepik.sql
@@ -1,9 +1,6 @@
 SELECT 
-    author,
-    title,
-    CASE 
-        WHEN author = 'Булгаков М.А.' THEN ROUND(price * 1.1, 2)
-        WHEN author = 'Есенин С.А.' THEN ROUND(price * 1.05, 2)
-        ELSE price
-    END AS new_price
-FROM book;
+	author,
+	title,
+	price
+FROM book
+WHERE amount < 10
