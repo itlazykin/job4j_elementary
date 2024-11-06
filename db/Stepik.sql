e1ed3e0a2b175103a3c1989e670efbb1dbc5e3d5
itlazykin <dzaka89@gmail.com>
2024-11-06 19:40:29 +0300

    Учу sql на stepik. Выборка данных, групповые функции MIN, MAX и AVG.

--- db/Stepik.sql
+++ db/Stepik.sql
@@ -1,2 +1,8 @@
-SELECT DISTINCT amount
-FROM book;
+SELECT 
+	author, 
+	MIN(price) AS "Минимальная_цена",
+	MAX(price) AS "Максимальная_цена ", 
+	ROUND(AVG(price), 6) AS "Средняя_цена"
+FROM book
+GROUP BY author
+ORDER by author ASC
