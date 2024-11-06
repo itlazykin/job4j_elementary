54d2809bb8c2e11bfd9292852f06516c4eab1365
itlazykin <dzaka89@gmail.com>
2024-11-06 14:29:23 +0300

    Учу sql на stepik. Удалить строку.

--- db/Stepik.sql
+++ db/Stepik.sql
@@ -1,7 +1,2 @@
-SELECT 
-    title,
-    author
-FROM book
-WHERE title LIKE '_% %'  -- выводит книги название которых состоит из двух и более слов
-  AND author LIKE '%_С.%' -- инициалы автора содержат букву «С»
-ORDER BY title ASC;
+DELETE FROM book
+WHERE book_id = 5;
