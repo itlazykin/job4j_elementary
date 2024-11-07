b087fc4381ca08af92a1de566a6b33a8e3c9f17b
itlazykin <dzaka89@gmail.com>
2024-11-07 10:29:31 +0300

    Учу sql на stepik. Выборка данных по условию, групповые функции. Применение HAVING.

--- db/Stepik.sql
+++ db/Stepik.sql
@@ -1,5 +1,7 @@
-SELECT
-	MIN(price) AS Минимальная_цена,
-	MAX(price) AS Максимальная_цена,
-	ROUND(AVG(price), 2) AS Средняя_цена
-FROM book
+SELECT ROUND(AVG(price), 2) AS Средняя_цена, SUM(price * amount) AS Стоимость
+FROM (
+    SELECT title, amount, price
+    FROM book
+    GROUP BY title, amount, price
+    HAVING amount > 4 AND amount < 15 --  В запросах с групповыми функциями вместо WHERE используется ключевое слово HAVING , которое размещается после оператора GROUP BY.
+) as _;
