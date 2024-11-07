fb0dd24ac02e90ffdcf7868bab7a588b569d1fcd
itlazykin <dzaka89@gmail.com>
2024-11-07 11:02:56 +0300

    Учу sql на stepik. Выборка данных по условию, групповые функции, WHERE и HAVING.

--- db/Stepik.sql
+++ db/Stepik.sql
@@ -1,7 +1,8 @@
-SELECT ROUND(AVG(price), 2) AS Средняя_цена, SUM(price * amount) AS Стоимость
-FROM (
-    SELECT title, amount, price
-    FROM book
-    GROUP BY title, amount, price
-    HAVING amount > 4 AND amount < 15 --  В запросах с групповыми функциями вместо WHERE используется ключевое слово HAVING , которое размещается после оператора GROUP BY.
-) as _;
+SELECT 
+	author,
+	SUM(amount * price) AS Стоимость
+FROM book
+WHERE title NOT IN ('Идиот', 'Белая гвардия')
+GROUP BY author
+HAVING SUM(price * amount) > 5000
+ORDER BY author DESC
