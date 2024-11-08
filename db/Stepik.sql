5c2967c783617754a44e1b87dc3d616f2359e39d
itlazykin <dzaka89@gmail.com>
2024-11-08 09:47:19 +0300

    Учу sql на stepik. 1.5 Запросы корректировки данных. Добавление записей в таблицу.

--- db/Stepik.sql
+++ db/Stepik.sql
@@ -1,7 +1,8 @@
-CREATE TABLE supply (
-	supply_id SERIAL PRIMARY KEY,  -- в mySQL для авто инкремента пишем: supply_id INT PRIMARY KEY AUTO_INCREMENT,
-	title VARCHAR(50),
-	author VARCHAR(30), 
-	price DECIMAL(8,2),
-	amount INT
-)
+INSERT INTO supply 
+	(title, author, price, amount)
+VALUES
+	('Лирика', 'Пастернак Б.Л.',518.99 ,2),
+	('Черный человек', 'Есенин С.А.',570.20 ,6),
+	('Белая гвардия', 'Булгаков М.А.',540.50 ,7),
+	('Идиот', 'Достоевский Ф.М.',360.80 ,3);
+SELECT * FROM supply
