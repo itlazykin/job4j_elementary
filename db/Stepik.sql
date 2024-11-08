43b12599b6c511a4414687352fc6f5d4229104bd
itlazykin <dzaka89@gmail.com>
2024-11-08 15:48:07 +0300

    Учу sql на stepik. 1.6 Таблица "Командировки", запросы на выборку. Задание 1.

--- db/Stepik.sql
+++ db/Stepik.sql
@@ -1,9 +1,9 @@
-CREATE TABLE ordering AS
-SELECT author, title,
-    (SELECT ROUND(AVG(amount)) 
-	FROM book
-	) AS amount
-FROM book
-WHERE amount < (SELECT AVG(amount) FROM book);
-
-SELECT * FROM ordering;
+/*
+ Вывести из таблицы trip информацию о командировках тех сотрудников,
+ фамилия которых заканчивается на букву «а», в отсортированном по убыванию даты последнего дня командировки виде.
+ В результат включить столбцы name, city, per_diem, date_first, date_last.
+ */
+SELECT name, city, per_diem, date_first, date_last
+FROM trip
+WHERE name LIKE '%а %'
+ORDER BY date_last DESC;
