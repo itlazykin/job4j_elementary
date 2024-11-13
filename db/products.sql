f0e5815537f05a15d8480ce16af93ce68fe9ef36
itlazykin <dzaka89@gmail.com>
2024-11-13 18:10:14 +0300

    6.3. Предложение DISTINCT 3

--- db/products.sql
+++ db/products.sql
@@ -1,15 +1,17 @@
 /*
-Предложение DISTINCT используется для удаления дубликатов из набора результатов выборки.
-Предложение DISTINCT можно использовать только с операторами SELECT. Синтаксис следующий:
+Для того чтобы в выборку попали данные из нескольких столбцов, при этом уникальность учитывалась только лишь в части
+из них - используется предложение DISTINCT. Синтаксис выглядит следующим образом:
 
-SELECT DISTINCT имена_столбцов_через_запятую FROM название_таблицы;
+SELECT DISTINCT ON (имена_столбцов_через_запятую) имена_столбцов_через_запятую_отражаем_в_выборке FROM название_таблицы;
 
-Если в предложении DISTINCT указан только один столбец для выборки, то запрос вернет уникальные значения для этого столбца.
+Хорошей практикой является всегда использовать ORDER BY вместе с DISTINCT чтобы сделать результирующий набор предсказуемым.
+При этом если в ORDER BY указано несколько столбцов, то самым левым в перечислении столбцов должно быть то,
+которое указано в DISTINCT - т.е. столбцы указанные в DISTINCT должны быть в ORDER BY (в любом порядке) и
+только потом уже могут идти остальные столбцы. В противном случае вы получите ошибку синтаксиса.
 
-Для представленной ниже схемы accounts напишите запрос, который выберет только уникальные записи по полю name.
-
-Для представленной ниже схемы products напишите запрос, который выберет только уникальные записи по полю name и price.
-Результирующая выборка должна быть отсортирована по полю price по убыванию.
+Для представленной ниже схемы products напишите запрос, который выполнит выборку уникальных строк по столбцу price.
+В выборке должны быть отображены все столбцы (name, price и count) ,
+сортировка должна быть по полям name, price и count.
 */
 
 CREATE TABLE products(
@@ -20,9 +22,13 @@ CREATE TABLE products(
 );
 
 INSERT INTO products VALUES(1, 'bread', 50, 10);
-INSERT INTO products VALUES(2, 'cheese', 250, 8);
-INSERT INTO products VALUES(3, 'milk', 75, 15);
-INSERT INTO products VALUES(4, 'butter', 115, 7);
-INSERT INTO products VALUES(5, 'sugar', 105, 30);
+INSERT INTO products VALUES(2, 'milk', 75, 15);
+INSERT INTO products VALUES(3, 'sugar', 105, 30);
+INSERT INTO products VALUES(4, 'bread', 50, 10);
+INSERT INTO products VALUES(5, 'milk', 68, 15);
+INSERT INTO products VALUES(6, 'sugar', 105, 30);
+INSERT INTO products VALUES(7, 'bread', 45, 10);
+INSERT INTO products VALUES(8, 'milk', 75, 15);
+INSERT INTO products VALUES(9, 'sugar', 105, 30);
 
-SELECT DISTINCT name, price FROM products ORDER BY price DESC;
+SELECT DISTINCT ON (price) name, price, count FROM products ORDER BY price, name, count;
