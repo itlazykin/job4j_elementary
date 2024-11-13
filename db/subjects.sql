e07d36cda8247e7864dd25fadf8d75b0151c0275
itlazykin <dzaka89@gmail.com>
2024-11-13 17:36:23 +0300

    1.3. Выбор всех полей с их названиями 3

---
+++ db/subjects.sql
@@ -0,0 +1,20 @@
+/*
+Ваша задача - написать запрос, который выполнит выборку всех записей из таблицы subjects,
+при этом в выборке должны быть представлены данные всех полей таблицы.
+Поля можете указывать в произвольном порядке. Схема таблицы представлена ниже.
+*/
+
+CREATE TABLE subjects(
+    id int not null primary key,
+    name text,
+    grade int,
+    start_date timestamp
+);
+
+INSERT INTO subjects VALUES(1, 'Math', 50, current_date);
+INSERT INTO subjects VALUES(2, 'English', 75, current_date);
+INSERT INTO subjects VALUES(3, 'Sociology', 65, current_date);
+INSERT INTO subjects VALUES(4, 'Economic', 60, current_date);
+INSERT INTO subjects VALUES(5, 'Computer Science', 95, current_date);
+
+select * from subjects
