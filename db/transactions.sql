2cb6bf3af9edad13e2adcc9d2a082ad465b41f96
itlazykin <dzaka89@gmail.com>
2024-11-18 17:15:45 +0300

    7.15. Агрегатные функции. MIN

--- db/transactions.sql
+++ db/transactions.sql
@@ -1,13 +1,13 @@
 /*
-Агрегатную функцию MAX можно использовать вместе с GROUP BY, чтобы получить максимальное значение для каждой группы.
-Таким образом можно, например, получить самую высокую цену за товар в определенной категории и т.п.
+Функция MIN - агрегатная функция, которая возвращает минимальное значение в наборе.
+Для того чтобы найти минимальное значение в столбце таблицы, мы можем просто передавать имя этого столбца в функцию MIN.
+Синтаксис выполнения имеет следующий вид:
 
-Примерный синтаксис такого запроса будет иметь вид:
+SELECT MIN(имя_столбца) FROM название_таблицы;
 
-SELECT столбец_группировки, остальные_столбцы, MAX(имя_столбца) FROM название_таблицы GROUP BY столбец_группировки;
+При этом тип данных столбца может быть числом, строкой или любым сопоставимым типом.
 
-Для представленной ниже схемы transactions напишите запрос, который найдет максимальное значение в столбце
-amount для каждой из групп. Данные должны быть сгруппированы по столбцу purpose.
+Для представленной ниже схемы transactions напишите запрос, который ищет минимальное значение в столбце amount.
 */
 
 CREATE TABLE transactions(
@@ -20,15 +20,12 @@ CREATE TABLE transactions(
 
 INSERT INTO transactions VALUES(1, 'Credit', 1, 132, '2022-06-01 12:00:00');
 INSERT INTO transactions VALUES(2, 'Current payment', 2, 94, '2022-06-01 12:00:00');
-INSERT INTO transactions VALUES(3, 'Service', 3, 194, '2022-06-01 12:00:00');
-INSERT INTO transactions VALUES(4, 'Vacation', 4, 254, '2022-06-01 12:00:00');
-INSERT INTO transactions VALUES(5, 'Credit', 1, 244, '2022-06-01 12:00:00');
-INSERT INTO transactions VALUES(6, 'Current payment', 2, 114, '2022-06-01 12:00:00');
-INSERT INTO transactions VALUES(7, 'Service', 3, 178, '2022-06-01 12:00:00');
-INSERT INTO transactions VALUES(8, 'Vacation', 4, 196, '2022-06-01 12:00:00');
-INSERT INTO transactions VALUES(9, 'Credit', 1, 198, '2022-06-01 12:00:00');
-INSERT INTO transactions VALUES(10, 'Current payment', 2, 78, '2022-06-01 12:00:00');
-INSERT INTO transactions VALUES(11, 'Service', 3, 156, '2022-06-01 12:00:00');
-INSERT INTO transactions VALUES(12, 'Vacation', 4, 152, '2022-06-01 12:00:00');
+INSERT INTO transactions VALUES(3, 'Service', 3, 197, '2022-06-01 12:00:00');
+INSERT INTO transactions VALUES(4, 'Vacation', 4, 253, '2022-06-01 12:00:00');
+INSERT INTO transactions VALUES(5, 'Delivery', 3, 167, '2022-06-01 12:00:00');
+INSERT INTO transactions VALUES(6, 'Current payment', 4, 84, '2022-06-01 12:00:00');
+INSERT INTO transactions VALUES(7, 'Service', 1, 132, '2022-06-01 12:00:00');
+INSERT INTO transactions VALUES(8, 'Credit', 2, 672, '2022-06-01 12:00:00');
+INSERT INTO transactions VALUES(9, 'Vacation', 1, 206, '2022-06-01 12:00:00');
 
-SELECT purpose, MAX(amount) FROM transactions GROUP BY purpose;
+SELECT MIN(amount) FROM transactions;
