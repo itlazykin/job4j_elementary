520d2d7d44f93340f645b630e548c0ae3a6a5350
itlazykin <dzaka89@gmail.com>
2024-11-18 17:04:55 +0300

    7.10. Агрегатные функции. SUM

--- db/transactions.sql
+++ db/transactions.sql
@@ -1,14 +1,15 @@
 /*
-Для того чтобы посчитать сумму всех значений в каком-нибудь столбце можно использовать функцию SUM.
-Если несколько значений в столбце совпадают, то для того, чтобы посчитать сумму среди уникальных значений,
-то можно просто добавить DISTINCT. Выполнить запрос можно следующим образом:
+Чтобы рассчитать сумму значений сгруппированных данных, вместе с SUM используем предложение GROUP BY - запрос будет
+выглядеть следующим образом:
 
-SELECT SUM(DISTINCT имя_столбца) FROM название_таблицы;
+SELECT столбец_группировки, остальные_столбцы, SUM(имя_столбца) FROM название_таблицы GROUP BY столбец_группировки;
 
-Функция SUM игнорирует NULL. Т.е. эта функция не учитывает NULL при расчетах.
+Вначале предложение GROUP BY делит данные таблицы на группы, а далее SUM выполняет расчет суммы значений для каждой из
+групп.
 
-Для представленной ниже схемы transactions выполните запрос, который вернет общую сумму уникальных значений в столбце
-amount.
+Для представленной ниже схемы transactions напишите запрос, который вернет сумму значений по столбцу amount.
+Данные должны быть сгруппированы по столбцу purpose, а также отсортированы по этому столбцу по убыванию.
+В результатах выборки должны быть только столбец purpose и SUM.
 */
 
 CREATE TABLE transactions(
@@ -21,12 +22,15 @@ CREATE TABLE transactions(
 
 INSERT INTO transactions VALUES(1, 'Credit', 1, 132, '2022-06-01 12:00:00');
 INSERT INTO transactions VALUES(2, 'Current payment', 2, 94, '2022-06-01 12:00:00');
-INSERT INTO transactions VALUES(3, 'Service', 3, 197, '2022-06-01 12:00:00');
-INSERT INTO transactions VALUES(4, 'Vacation', 4, 253, '2022-06-01 12:00:00');
-INSERT INTO transactions VALUES(5, 'Delivery', 3, 167, '2022-06-01 12:00:00');
-INSERT INTO transactions VALUES(6, 'Current payment', 4, 84, '2022-06-01 12:00:00');
-INSERT INTO transactions VALUES(7, 'Service', 1, 132, '2022-06-01 12:00:00');
-INSERT INTO transactions VALUES(8, 'Credit', 2, 672, '2022-06-01 12:00:00');
-INSERT INTO transactions VALUES(9, 'Vacation', 1, 206, '2022-06-01 12:00:00');
+INSERT INTO transactions VALUES(3, 'Service', 3, 194, '2022-06-01 12:00:00');
+INSERT INTO transactions VALUES(4, 'Vacation', 4, 254, '2022-06-01 12:00:00');
+INSERT INTO transactions VALUES(5, 'Credit', 1, 244, '2022-06-01 12:00:00');
+INSERT INTO transactions VALUES(6, 'Current payment', 2, 114, '2022-06-01 12:00:00');
+INSERT INTO transactions VALUES(7, 'Service', 3, 178, '2022-06-01 12:00:00');
+INSERT INTO transactions VALUES(8, 'Vacation', 4, 196, '2022-06-01 12:00:00');
+INSERT INTO transactions VALUES(9, 'Credit', 1, 198, '2022-06-01 12:00:00');
+INSERT INTO transactions VALUES(10, 'Current payment', 2, 78, '2022-06-01 12:00:00');
+INSERT INTO transactions VALUES(11, 'Service', 3, 156, '2022-06-01 12:00:00');
+INSERT INTO transactions VALUES(12, 'Vacation', 4, 152, '2022-06-01 12:00:00');
 
-SELECT SUM(DISTINCT amount) FROM transactions;
+SELECT purpose, SUM(amount) FROM transactions GROUP BY purpose ORDER BY purpose DESC;
