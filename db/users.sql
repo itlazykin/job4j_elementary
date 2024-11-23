19efa163572ad3c8a6d01dd0e080d8977e35bfcb
itlazykin <dzaka89@gmail.com>
2024-11-23 16:31:04 +0300

    4. INNER JOIN 4

--- db/users.sql
+++ db/users.sql
@@ -3,39 +3,50 @@
 Например, есть 2 таблицы:
 
 CREATE TABLE A (
-    pka int primary key,
+    id int primary key,
     c1 text
 );
 
 CREATE TABLE B (
-    pkb int primary key,
+    id int primary key,
     c2 text,
-    fka int references A(pka)
+    a_id int references A(id)
 );
 
-Синтаксис выполнения SELECT запроса с использование INNER JOIN будет иметь вид:
+Обратите внимание, что при соединении таблиц мы столкнемся с тем, что будут столбцы в разных таблицах иметь одинаковые
+имена. Таким образом, если мы ссылаемся на столбцы с одинаковыми именами из разных таблиц в запросе,
+то мы получим ошибку.
+Чтобы избежать ошибки, нам необходимо полностью обозначить эти столбцы используя следующий синтаксис:
 
-SELECT pka, c1, pkb, c2
-FROM A
-INNER JOIN B ON pka = fka;
-Как же это работает?
+название_таблицы.название_столбца
 
-Для каждой строки в таблице А внутреннее соединение сравнивает значение в столбце pka со значением в столбце fka каждой строки в таблице В:
-- если эти значения равны, то внутреннее соединение создает новую строку, которая содержит столбцы обеих таблиц и добавляет ее в результаты выборки;
-- если же значения не равны, то внутреннее соединение просто игнорирует их и переходит к следующей строке.
+На практике, чтобы не писать полное имя таблицы, используются их псевдонимы для того, чтобы присвоить короткие имена
+объединенным таблицам и повысить читабельность запроса.
+С учетом всего указанного запрос для таблиц А и В будет иметь вид:
 
-Для представленной ниже схемы напишите запрос, который вернет все строки, в результатах будут отражены данные столбцов
-id, login, password, email, name и объединение будет происходить по id и account_id;
+SELECT a.id, a.c1, b.id, b.c2
+FROM A AS a
+INNER JOIN B b on a.id = b.a_id
+
+Обратите внимание, что можно указывать AS для присвоения псевдонима, но это не является обязательным
+(продемонстрировано для таблицы B). Также стоит подчеркнуть, что для идентификации столбцов указывать псевдоним
+необходимо только там, где имена столбцов совпадают.
+
+Для представленной ниже схемы напишите запрос, который вернет все строки из двух таблиц, в результатах должны быть
+отражены значения столбцов id из accounts, email из accounts, password из accounts, email из users, name из users и
+объединение будет по столбцам id из accounts и account_id  из users. В качестве псевдонимов используйте первые буквы
+названия таблиц.
+
 */
 
 CREATE TABLE accounts (
     id int primary key,
-    login text,
+    email text,
     password text
 );
 
 CREATE TABLE users (
-    u_id int primary key,
+    id int primary key,
     email text,
     name text,
     account_id int references accounts(id)
@@ -47,6 +58,6 @@ INSERT INTO accounts VALUES (2, 'login_2', '67890');
 INSERT INTO users VALUES (1, 'email_1@mail.ru', 'first_name', 1);
 INSERT INTO users VALUES (2, 'email_2@bk.ru', 'second_name', 2);
 
-SELECT id, login, password, email, name
-FROM accounts
-INNER JOIN users ON id = account_id
+SELECT a.id, a.email, a.password, u.email, u.name
+FROM accounts AS a
+INNER JOIN users AS u ON a.id = u.account_id
