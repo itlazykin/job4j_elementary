ac2a59eee0da9c770c3794887aed71b95b369758
itlazykin <dzaka89@gmail.com>
2024-11-28 16:40:51 +0300

    12. RIGHT JOIN 3

--- db/accounts.sql
+++ db/accounts.sql
@@ -1,38 +1,42 @@
 /*
 Для того чтобы получить данные из нескольких таблиц, используется JOIN. При этом бывают не только внутренние,
-но и внешние JOIN. В свою очередь они бывают разных видов. Рассмотрим LEFT JOIN.
+но и внешние JOIN. В свою очередь они бывают разных видов. Рассмотрим RIGHT JOIN.
 
-CREATE TABLE A (
-    pka int primary key,
-    c1 text
+Например, у нас есть следующие таблицы:
+
+CREATE TABLE films (
+    film_id int  PRIMARY KEY,
+    title text
 );
 
-CREATE TABLE B (
-    pkb int primary key,
-    c2 text,
-    fka int references A(pka)
+CREATE TABLE film_reviews (
+    review_id int PRIMARY KEY,
+    review text,
+    film_id int
 );
+Запрос с использованием будет иметь следующий вид:
 
-Каждая строка в таблице А может иметь много соответствующих строк в таблице В или не иметь вообще.
-В это же время каждая строка в таблице В, имеет одну и только одну соответствующую в таблице А.
-Синтаксис выполнения LEFT JOIN будет иметь следующий вид:
+SELECT review, title
+FROM films
+RIGHT JOIN film_reviews on films.film_id = film_reviews.film_id;
 
-SELECT pka, c1, pkb, c2
-FROM A
-LEFT JOIN B ON pka = fka;
+Каким же образом это работает?
+- предложение RIGHT JOIN начинает выборку данных из правой таблицы – film_reviews;
+- для каждой строки из правой таблицы (film_reviews) RIGHT JOIN проверяет, равно ли значение
+film_id столбце таблицы film_reviews значению в столбце film_id каждой строки из левой таблицы (films);
+- если указанные значения равны, то RIGHT JOIN создает новую строку, которая содержит значения столбцов из обеих таблиц,
+ которые указаны в SELECT и включает эту строку в результаты выборки;
+- если же указанные значения не равны, то RIGHT JOIN все равно создает новую строку,
+которая содержит столбцы из обеих таблиц и эта новая строка включается в результаты выборки.
+Однако в таком случае столбцы из левой таблицы films значением NULL.
+- таким образом, RIGHT JOIN выбирает все строки из правой таблицы независимо от того,
+есть ли у них совпадающие строки из левой таблицы.
 
-Каким же образом это работает? LEFT JOIN начинает выборку данных из левой таблицы. Для каждой строки в
-левой таблице выполняется сравнение значение в столбце pka со значением каждой строки в столбце fka правой таблицы:
-- если значения равны, то LEFT JOIN создает новую строку, в которой содержатся столбцы, обозначенные в предложении
-SELECT и далее строка добавляется в результирующий набор;
-- если значения не равны, то LEFT JOIN также создает новую строку, в которой содержатся столбцы, обозначенные
-в предложении SELECT. Поскольку в таком случае из правой таблицы мы не получаем совпадение,
-то столбцы из правой таблицы заполняются значением NULL.
-
-Для представленной ниже схемы напишите запрос с использованием LEFT JOIN. Левая таблица – таблица accounts.
-Объединение осуществляется по столбцам id из таблицы accounts и account_id. В результатах выборки должны быть
-отражены значения столбцов id из таблицы accounts, email из таблицы accounts, password, id из таблицы users, email
-из таблицы users, name. В качестве псевдонимов используйте первую букву названия таблиц.
+Напишите запрос с использованием RIGHT JOIN для схемы, представленной ниже.
+В результатах должны быть отражены значения столбцов id из таблицы accounts,
+email из таблицы accounts, password, id из таблицы users, email из таблицы users, name.
+Объединением происходит по столбцу id из accounts и account_id из таблицы users.
+В качестве псевдонимов используйте первые буквы названия таблиц.
 */
 
 CREATE TABLE accounts (
@@ -48,14 +52,14 @@ CREATE TABLE users (
     account_id int references accounts(id)
 );
 
-INSERT INTO accounts VALUES (1, 'some_for_auth_1@mail.ru', '1_3_5');
-INSERT INTO accounts VALUES (2, 'some_for_auth_1@bk.ru', '6%8%0');
-INSERT INTO accounts VALUES (3, 'for_auth_1@mail.ru', '12345');
-INSERT INTO accounts VALUES (4, 'for_auth_1@bk.ru', '67890');
+INSERT INTO accounts VALUES (1, 'for_auth_1@mail.ru', '12345');
+INSERT INTO accounts VALUES (2, 'for_auth_1@bk.ru', '67890');
 
-INSERT INTO users VALUES (1, 'email_1@mail.ru', 'first_name', 3);
-INSERT INTO users VALUES (2, 'email_2@bk.ru', 'second_name', 4);
+INSERT INTO users VALUES (1, 'email_1@mail.ru', 'first_name', 1);
+INSERT INTO users VALUES (2, 'email_2@bk.ru', 'second_name', null);
+INSERT INTO users VALUES (3, 'email_3@mail.ru', 'third_name', null);
+INSERT INTO users VALUES (4, 'email_4@bk.ru', 'fourth_name', 2);
 
 SELECT a.id, a.email, a.password, u.id, u.email, u.name
 FROM accounts AS a
-LEFT JOIN users AS u ON a.id = u.account_id
+RIGHT JOIN users AS u ON a.id = account_id
