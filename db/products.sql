849891fc13d4d5a6656566956753583cde3bca52
itlazykin <dzaka89@gmail.com>
2024-12-06 18:21:14 +0300

    20. GROUP BY и WHERE 10

--- db/products.sql
+++ db/products.sql
@@ -1,7 +1,9 @@
 /*
 GROUP BY позволяет разделить данные, которые возвращены с помощью SELECT на группы.
 При этом для каждой из групп можно будет использовать агрегатные функции, например COUNT() для подсчета предметов в
-каждой из групп. Синтаксис имеет следующий вид:
+каждой из групп. Как и в других запросах, с GROUP BY можно использовать WHERE. Важно уяснить правило - WHERE
+обрабатывается после FROM и до GROUP BY. Таким образом, данные сначала фильтруются, а потом выполняется их группировка.
+Синтаксис имеет следующий вид:
 
 SELECT
     столбец_1,
@@ -18,45 +20,25 @@ GROUP BY
 Необходимо учесть - любой столбец, который указан в SELECT
 (столбец, который хранит результат вычисления агрегатных функций, не считается), должен быть указан после GROUP BY.
 
-Создайте запрос, который вычислит общий доход от продаж для каждой категории товаров. Используйте таблицы:
-- "Продукты" с полями: товар, категория, цена. - "Продажи" с полями: продажа, id товара, количество.
+Создайте запрос, который определит количество продуктов в каждой категории с ценой выше средней цены по всем продуктам.
 Группировка будет по category.
 */
 
 CREATE TABLE products
 (
-    id INT PRIMARY KEY,
-    category   VARCHAR(50),
-    price      INT
+    id       SERIAL PRIMARY KEY,
+    name     VARCHAR(100),
+    category VARCHAR(50),
+    price    INT
 );
 
-CREATE TABLE sales
-(
-    sale_id    INT PRIMARY KEY,
-    product_id INT references products(id),
-    quantity   INT
-);
+INSERT INTO products (name, category, price)
+VALUES ('Laptop', 'Electronics', 1200),
+       ('T-shirt', 'Clothing', 30),
+       ('Smartphone', 'Electronics', 800),
+       ('Book', 'Books', 20);
 
-INSERT INTO products
-VALUES (1, 'Electronics', 500),
-       (2, 'Clothing', 30),
-       (3, 'Electronics', 700),
-       (4, 'Books', 20);
-
-INSERT INTO sales
-VALUES (1, 1, 10),
-       (2, 2, 5),
-       (3, 3, 8),
-       (4, 4, 12);
-
-SELECT
-    p.category AS category,
-    SUM(p.price * s.quantity) AS sum
-FROM
-    products p
-JOIN
-    sales s
-ON
-    p.id = s.product_id
-GROUP BY
-    p.category;
+SELECT category, count(price) AS count
+FROM products
+WHERE price > (SELECT AVG(price) FROM products)
+GROUP BY category
