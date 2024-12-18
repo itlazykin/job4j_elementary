b25c6f8e562327e0531506fde2d8c41f56e50de7
itlazykin <dzaka89@gmail.com>
2024-12-18 16:06:40 +0300

    17. Подзапрос в SELECT 9. Правки по задаче.

--- db/products.sql
+++ db/products.sql
@@ -33,65 +33,14 @@ HAVING
 
 CREATE TABLE products
 (
-    id    SERIAL PRIMARY KEY,
-    name  VARCHAR(255)   NOT NULL,
-    price DECIMAL(10, 2) NOT NULL
+    id           SERIAL PRIMARY KEY,
+    product_name VARCHAR(100),
+    price        DECIMAL(10, 2)
 );
 
-CREATE TABLE customers
-(
-    id   SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL
-);
-
-CREATE TABLE sales
-(
-    id     SERIAL PRIMARY KEY,
-    product_id  INT REFERENCES products (id),
-    customer_id INT REFERENCES customers (id),
-    amount      INT NOT NULL,
-    sale_year   INT,
-    sale_month  INT
-);
-
-INSERT INTO products (name, price)
-VALUES ('Product A', 10.99),
-       ('Product B', 20.49),
-       ('Product C', 15.75),
-       ('Product D', 8.99),
-       ('Product E', 12.50),
-       ('Product F', 18.99),
-       ('Product G', 22.00);
-
-INSERT INTO customers (name)
-VALUES ('Customer 1'),
-       ('Customer 2'),
-       ('Customer 3'),
-       ('Customer 4'),
-       ('Customer 5');
-
-INSERT INTO sales (product_id, customer_id, amount, sale_year, sale_month)
-VALUES (1, 1, 2, 2023, 1),
-       (2, 2, 1, 2023, 2),
-       (3, 3, 3, 2023, 3),
-       (4, 4, 4, 2023, 4),
-       (5, 5, 2, 2023, 5),
-       (6, 1, 1, 2023, 4),
-       (7, 2, 3, 2023, 3),
-       (1, 3, 2, 2023, 1),
-       (2, 4, 4, 20235, 5),
-       (3, 5, 1, 2023, 2),
-       (4, 1, 3, 2023, 1),
-       (5, 2, 2, 2023, 3),
-       (6, 3, 1, 2023, 5),
-       (7, 4, 4, 2023, 4),
-       (1, 5, 3, 2023, 2),
-       (2, 1, 2, 2023, 1),
-       (3, 2, 1, 2023, 3);
-
-SELECT p.id, p.name
-FROM products p
-JOIN sales s ON p.id = s.product_id
-WHERE sale_year = 2023
-GROUP BY p.id, p.name
-HAVING COUNT(s.id) = 2
+INSERT INTO products (product_name, price)
+VALUES ('Товар 1', 10.00),
+       ('Товар 2', 15.50),
+       ('Товар 3', 20.25),
+       ('Товар 4', 25.75),
+       ('Товар 5', 30.20);
