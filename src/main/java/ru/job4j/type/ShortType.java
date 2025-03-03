2175b7af02be051976f4d4df7b585c98b8b404f0
itlazykin <dzaka89@gmail.com>
2025-03-03 15:51:06 +0300

    2. Создание short переменной

--- src/main/java/ru/job4j/type/ShortType.java
+++ src/main/java/ru/job4j/type/ShortType.java
@@ -2,6 +2,9 @@ package ru.job4j.type;
 
 import java.util.Scanner;
 
+/**
+ * Программа получает от пользователя число в диапазоне short и выводит его на консоль.
+ */
 public class ShortType {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
