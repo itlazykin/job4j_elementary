b46041c1cf0dedb5a1e7941ea373852dc62bd397
itlazykin <dzaka89@gmail.com>
2025-03-03 15:52:57 +0300

    4. Целочисленное деление

--- src/main/java/ru/job4j/type/DivMod.java
+++ src/main/java/ru/job4j/type/DivMod.java
@@ -1,5 +1,8 @@
 package ru.job4j.type;
 
+/**
+ * Поделить число 11 нацело на 3 и получить остаток от деления от 3
+ */
 public class DivMod {
     public static void main(String[] args) {
         int num = 11;
