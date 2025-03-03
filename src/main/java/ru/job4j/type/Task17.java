aad858c942eab0333680a625dc83b82bcc0994ac
itlazykin <dzaka89@gmail.com>
2025-03-03 16:41:44 +0300

    17. Высчитать площадь равнобедренного треугольника

---
+++ src/main/java/ru/job4j/type/Task17.java
@@ -0,0 +1,13 @@
+package ru.job4j.type;
+
+/**
+ * Написать код, который рассчитывает площадь этого равнобедренного треугольника. Также периметр прямоугольника,
+ * сторонами которого являются основание и высота описанного в задаче треугольника.
+ */
+public class Task17 {
+    public static void calculate(int base, int height) {
+        double area = 0.5 * base * height;
+        double perimeter = 2 * (base + height);
+        System.out.printf("%.1f%n%.1f", area, perimeter);
+    }
+}
