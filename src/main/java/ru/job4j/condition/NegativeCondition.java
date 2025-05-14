ed6e14d0c98a54a252771bdb853b73369cf1667a
Denis Lazykin <dzaka89@gmail.com>
2025-05-14 20:57:11 +0300

    Упражнения Оператор ветвления 1. Логические выражения

---
+++ src/main/java/ru/job4j/condition/NegativeCondition.java
@@ -0,0 +1,14 @@
+package ru.job4j.condition;
+
+/**
+ * Запишите два логических условия. Сначала что число отрицательное, а потом что положительное
+ */
+public class NegativeCondition {
+    public static void main(String[] args) {
+        int num = -1;
+        boolean result1 = true;
+        boolean result2 = false;
+        System.out.println(result1);
+        System.out.println(result2);
+    }
+}
